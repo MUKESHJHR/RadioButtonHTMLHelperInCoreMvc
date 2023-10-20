f05710406435354e1915b150fb363502b40ab705
mukesh kumar <mukesh.1006jhr@outlook.com>
2023-10-21 04:47:47 +0530

    Add project files.

---
+++ RadioButtonHTMLHelperInCoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RadioButtonHTMLHelperInCoreMvc.Models;
+using System.Diagnostics;
+
+namespace RadioButtonHTMLHelperInCoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Company company = new Company();
+            //company.SelectedDepartment = 2;
+
+            return View(company);
+        }
+
+        [HttpPost]
+        public string Index(Company company)
+        {
+            if (company.SelectedDepartment <= 0)
+            {
+                return "You did not select any department";
+            }
+            else
+            {
+                return "You selected department with Id = " + company.SelectedDepartment;
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RadioButtonHTMLHelperInCoreMvc/Models/Company.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace RadioButtonHTMLHelperInCoreMvc.Models
+{
+    public class Company
+    {
+        public int SelectedDepartment { get; set; }
+
+        public List<Department> Departments()
+        {
+            List<Department> ListDepartments = new List<Department>()
+            {
+                new Department() {Id = 1, Name="IT" },
+                new Department() {Id = 2, Name="HR" },
+                new Department() {Id = 3, Name="Manager" },
+            };
+
+            return ListDepartments;
+        }
+    }
+}
